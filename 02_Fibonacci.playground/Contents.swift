0d6e3db514aa12131f961dd971528eb6a18d5675
seabrea <hgdigm@gmail.com>
2020-02-29 14:19:58 +0800

    动态数组

--- 02_Fibonacci.playground/Contents.swift
+++ 02_Fibonacci.playground/Contents.swift
@@ -10,6 +10,24 @@ func fibonacci (num: Int) -> Int {
     }
 }
 
+// 0 1 1 2 3 5 8 13
+func fibonacci (_ num: Int) -> Int {
+    if num < 2 {
+        return num
+    }
+    
+    var first = 0
+    var second = 1
+    for _ in 0..<(num-1) {
+        let sum = first + second
+        first = second
+        second = sum
+    }
+    
+    return second
+}
+
+//1 1 2 3 5 8 13 21 34 55
 print("######### 结果: #########")
 print(fibonacci(num: 1),fibonacci(num: 2),fibonacci(num: 3),fibonacci(num: 4),fibonacci(num: 5),fibonacci(num: 6),fibonacci(num: 7),fibonacci(num: 8),fibonacci(num: 9),fibonacci(num: 10))
 
---
+++ 03_MyMutableArray.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class MyMutableArray {
+    
+    func append(_ : Int) {
+        
+    }
+    
+    func remove(_ : Int) {
+        
+    }
+    
+    func clear(_ : Int) {
+        
+    }
+    
+    func size() -> Int {
+        return 0
+    }
+    
+    func isEmpty() -> Bool {
+        return true
+    }
+}
+
+let list = MyMutableArray()
+
+list.isEmpty()
+
